873c01b4b1ef215d2d95679b21a4ef201c72ee9f
the_samy <agramontesamy@gmail.com>
2020-08-19 14:25:56 -0400

    se creo las migraciones
    se agregaron las migraciones pertenecientes a categoria

---
+++ CyberNews/database/seeds/categoryTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class categoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('category')->insert([
+           'nombre'=>'Deportes',
+           'descripcion' => 'seccion de deportes',
+
+
+        ]);
+    }
+}
