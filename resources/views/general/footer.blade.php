417f7b8ec8d7f4f3fb5a984d7a5fc417284ca79a
Jesus Melo <jmelo@elcatador.com>
2022-02-17 20:40:18 -0400

    Responsive features.

--- resources/views/general/footer.blade.php
+++ resources/views/general/footer.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-8 col-md-10 mx-auto">
           <ul class="list-inline text-center">
             <li class="list-inline-item">
-              <a href="#">
+              <a href="#" target=_blank>
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="#" target=_blank>
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://instagram.com/jesus.raynieri" target=_blank>
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
@@ -29,7 +29,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://github.com/jr-melo" target=_blank>
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-github fa-stack-1x fa-inverse"></i>
@@ -37,7 +37,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="https://gitlab.com/jr.melo/CyberNews">
+              <a href="https://gitlab.com/jr.melo" target=_blank>
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-gitlab fa-stack-1x fa-inverse"></i>
--- resources/views/general/navigator.blade.php
+++ resources/views/general/navigator.blade.php
@@ -1,100 +1,111 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-  <div class="container">
-@if (Route::has('login'))
-<div class="top-right links">
-    @auth
-        <a class="navbar-brand" href="{{ url('/admin') }}">Admin</a>
+    <div class="container">
+        <!-- Navigation -->
 
-        <a class="navbar-brand" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    @else
-        <a class="navbar-brand" href="{{ route('login') }}">Login</a>
+        <a class="navbar-brand" href="{{ url('/') }}">CyberNews</a>
+        <!--<li class="nav-item has-treeview list-unstyled">
+            <a href="#" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav nav-treeview navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/categories') }}">Publicaciones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/about') }}">Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/contact') }}">Contacto</a>
+                    </li>
+                    @if (Route::has('login'))
+                        {{-- <div class="top-right links"> --}}
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/admin') }}">Admin</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                        @endauth
+                        {{-- </div> --}}
+                    @endif
+                    {{-- </ul> --}}
+                </ul>
+            </div>
 
-    @endauth
-</div>
-@endif
- <!-- Navigation -->
- 
-      <a class="navbar-brand" href="{{ url('/') }}">CyberNews</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/') }}">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/categories') }}">Publicaciones</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/about') }}">Nosotros</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/contact') }}">Contacto</a>
-          </li>
-        </ul>
-      </div>
+        </li> -->
     </div>
-  </nav>
+</nav>
 
-  <!-- Styles -->
+<!-- Styles -->
 <style>
-  html, body {
-      background-color: #fff;
-      color: #636b6f;
-      font-family: 'Nunito', sans-serif;
-      font-weight: 200;
-      height: 100vh;
-      margin: 0;
-  }
+    html,
+    body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    }
 
-  .full-height {
-      height: 100vh;
-  }
+    /*  .full-height {
+        height: 100vh;
+    } */
 
-  .flex-center {
-      align-items: center;
-      display: flex;
-      justify-content: center;
-  }
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
 
-  .position-ref {
-      position: relative;
-  }
+    .position-ref {
+        position: relative;
+    }
 
-  .top-right {
-      position: absolute;
-      right: 10px;
-      top: 18px;
-  }
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
 
-  .content {
-      text-align: center;
-  }
+    .content {
+        text-align: center;
+    }
 
-  .title {
-      font-size: 84px;
-  }
+    .title {
+        font-size: 84px;
+    }
 
-  .links > a {
-      color: #636b6f;
-      padding: 0 25px;
-      font-size: 13px;
-      font-weight: 600;
-      letter-spacing: .1rem;
-      text-decoration: none;
-      text-transform: uppercase;
-  }
+    .links>a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
 
-  .m-b-md {
-      margin-bottom: 30px;
-  }
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+
 </style>
